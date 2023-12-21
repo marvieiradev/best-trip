f6fdf6214b41da2c2908b60489eb69580ba8344c
Marcos Vieira <marcosvieira482@gmail.com>
2023-12-21 13:01:52 -0300

    feat: add tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,18 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "search-background": "url(/world-map.png)",
+      },
+      colors: {
+        primary: "#590BD8",
+        primaryLighter: "#DDD5EA",
+        primaryDarker: "#312A4F",
+        grayPrimary: "#717171",
+        grayLighter: "#BBBFBF",
+        walterWhite: "#F5F5F5",
+      },
+      textColor: {
+        dark: "#717171",
       },
     },
   },
