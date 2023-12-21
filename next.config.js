ae4585c0c07a2bd495297f372e61dfc4f23540ab
Marcos Vieira <marcosvieira482@gmail.com>
2023-12-21 18:56:16 -0300

    feat: add trip item component

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [{
-            protocol: 'https',
-            hostname: 'lh3.googleusercontent.com',
-            port: '',
-        }],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+            },
+        ],
     },
 }
 
