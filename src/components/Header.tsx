b194cb80dbeed9faa595c2037021c1503cb63d35
Marcos Vieira <marcosvieira482@gmail.com>
2023-12-22 14:34:07 -0300

    feat: add trip location component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,7 +35,7 @@ const Header = () => {
                     <Image height={32} width={32} src={data.user.image!} alt={data.user.name!} className="rounded-full shadow-md" />
 
                     {menuIsOpen && (
-                        <div className="absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
+                        <div className="z-50 absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
                             <button className="text-primary text-sm font-semibold" onClick={handleLogoutClick}>
                                 <div className="flex flex-row items-center gap-2">
                                     <CiLogout size={16} className="text-primary" />
