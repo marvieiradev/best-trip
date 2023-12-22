d583748606749c7fe462357100d9602f71c1bdd2
Marcos Vieira <marcosvieira482@gmail.com>
2023-12-22 01:21:36 -0300

    feat: update loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+
 const Loading = () => {
-    return (<div>Carregando...</div>);
+    return (
+        <div className="flex h-full flex-col items-center justify-center gap-2 p-5 mt-[15%]">
+            <h2 className="font-bold text-primaryDarker">Aguarde...</h2>
+            <Image src={"/icon.svg"} width={40} height={40} alt="loading" className="anim" />
+        </div>
+
+    );
 }
 
 export default Loading;
