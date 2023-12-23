283013383d1af22061c07292b16a0df599b06a02
Marcos Vieira <marcosvieira482@gmail.com>
2023-12-23 12:56:04 -0300

    feat: add date validation

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -1,14 +1,12 @@
 "use client";
-
 import Button from "@/components/Button";
 import DatePicker from "@/components/DatePicker";
 import Input from "@/components/Input";
-import { Trip } from "@prisma/client";
 import { differenceInDays } from "date-fns";
-import { difference } from "next/dist/build/utils";
 import { Controller, useForm } from "react-hook-form";
 
 interface TripReservationProps {
+    tripId: string;
     tripStartDate: Date;
     tripEndDate: Date;
     maxGuests: number;
@@ -22,11 +20,45 @@ interface TripReservationForm {
 }
 
 
-const TripReservation = ({ maxGuests, tripStartDate, tripEndDate, pricePerDay }: TripReservationProps) => {
-    const { register, handleSubmit, formState: { errors }, control, watch } = useForm<TripReservationForm>();
-    const onSubmit = (data: any) => {
-        console.log({ data })
-    }
+const TripReservation = ({ tripId, maxGuests, tripStartDate, tripEndDate, pricePerDay }: TripReservationProps) => {
+    const { register, handleSubmit, formState: { errors }, control, watch, setError } = useForm<TripReservationForm>();
+    const onSubmit = async (data: any) => {
+        const response = await fetch('http://localhost:3000/api/trips/check', {
+            method: "POST",
+            body: Buffer.from(JSON.stringify({
+                startDate: data.startDate,
+                endDate: data.endDate,
+                tripId,
+            }))
+        })
+
+        const res = await response.json();
+        if (res?.error?.code === 'TRIP_ALREADY_RESERVED') {
+            setError("startDate", {
+                type: "manual",
+                message: "Esta data já está reservada."
+            });
+
+            setError("endDate", {
+                type: "manual",
+                message: "Esta data já está reservada."
+            });
+        }
+
+        if (res?.error?.code === 'INVALID_START_DATE') {
+            setError("startDate", {
+                type: "manual",
+                message: "Data inválida.",
+            });
+        }
+
+        if (res?.error?.code === 'INVALID_END_DATE') {
+            setError("endDate", {
+                type: "manual",
+                message: "Data inválida.",
+            });
+        }
+    };
 
     const startDate = watch("startDate");
     const endDate = watch("endDate");
@@ -109,7 +141,6 @@ const TripReservation = ({ maxGuests, tripStartDate, tripEndDate, pricePerDay }:
                 <div className="pb-10 border-b border-b-grayLighter w-full">
                     <Button onClick={() => handleSubmit(onSubmit)()} className="mt-3 w-full">Reservar agora</Button>
                 </div>
-
             </div>
         </div>
     );
--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -22,7 +22,7 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
     return (
         <div className="container mx-auto">
             <TripHeader trip={trip} />
-            <TripReservation maxGuests={trip.maxGuests} tripStartDate={trip.startDate} tripEndDate={trip.endDate} pricePerDay={Number(trip.pricePerDay)} />
+            <TripReservation tripId={trip.id} maxGuests={trip.maxGuests} tripStartDate={trip.startDate} tripEndDate={trip.endDate} pricePerDay={Number(trip.pricePerDay)} />
             <TripDescription description={trip.description} />
             <TripHighlights highlights={trip.highlights} />
             <TripLocation location={trip.location} locationDescription={trip.locationDescription} />
