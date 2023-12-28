e1ac1c7555b1e3b8a6634247691e0e925fc722b0
Marcos Vieira <marcosvieira482@gmail.com>
2023-12-28 16:20:48 -0300

    feat: update api route files

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -1,7 +1,7 @@
-import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     apiVersion: "2023-10-16",
