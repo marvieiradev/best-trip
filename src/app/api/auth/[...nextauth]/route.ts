b3368c8f17e531dd80b0d1a27c29c01c518da1cc
Marcos Vieira <marcosvieira482@gmail.com>
2023-12-28 14:47:12 -0300

    fix: authOptions deploy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,6 @@
-import NextAuth, { AuthOptions, NextAuthOptions } from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { Adapter } from "next-auth/adapters"
-import { prisma } from "@/lib/prisma"
+import NextAuth from "next-auth"
+import { authOptions } from "./options";
 
-const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma) as Adapter,
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-    ],
-    secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-        async session({ session, token, user }) {
-            session.user = { ...session.user, id: user.id } as { id: string; name: string; email: string };
-
-            return session;
-        },
-    },
-}
 
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST, authOptions }
