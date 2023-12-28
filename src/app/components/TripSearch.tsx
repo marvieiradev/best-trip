8ac1fdfc6b28d968a97997221c62ef5b369a182e
Marcos Vieira <marcosvieira482@gmail.com>
2023-12-28 04:02:39 -0300

    feat: add trip search

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -1,23 +1,62 @@
 'use client';
-
 import Button from "@/components/Button";
 import CurrencyInput from "@/components/CurrencyInput";
 import DatePicker from "@/components/DatePicker";
 import Input from "@/components/Input";
+import { useRouter } from "next/navigation";
+import { Controller, useForm } from "react-hook-form";
+
+interface TripSearchForm {
+    text: string;
+    startDate: Date | null;
+    budget: string;
+}
 
 const TripSearch = () => {
+    const router = useRouter();
+    const { control, formState: { errors }, register, handleSubmit } = useForm<TripSearchForm>();
+    const onSubmit = (data: TripSearchForm) => {
+        router.push(`/trips/search?text=${data.text}&startDate=${data.startDate?.toISOString()}&budget=${data.budget}`);
+    }
+
     return (
         <div className="contaniner mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat">
             <h1 className="font-semibold text-2xl text-primaryDarker text-center">
                 Encontre sua próxima <span className="text-primary">viagem!</span>
             </h1>
             <div className="flex flex-col gap-4 mt-5">
-                <Input placeholder="Onde você quer ir?" />
+                <Input placeholder="Onde você quer ir?"
+                    error={!!errors.text}
+                    errorMessage={errors.text?.message}
+                    {...register("text", {
+                        required: {
+                            value: true,
+                            message: "Texto é obrigatório.",
+                        }
+                    })} />
                 <div className="flex gap-4">
-                    <DatePicker placeholderText="Data de ida" onChange={() => { }} className="w-full" />
-                    <CurrencyInput placeholder="Orçamento" />
+                    <Controller
+                        name="startDate"
+                        control={control}
+                        render={({ field }) => (
+                            <DatePicker
+                                onChange={field.onChange}
+                                selected={field.value}
+                                placeholderText="Data Inicial"
+                                className="w-full"
+                                minDate={new Date()}
+                            />
+                        )}
+                    />
+                    <Controller
+                        name="budget"
+                        control={control}
+                        render={({ field }) => (
+                            <CurrencyInput allowDecimals={false} placeholder="Orçamento" onValueChange={field.onChange as any} value={field.value} onBlur={field.onBlur} />
+                        )}
+                    />
                 </div>
-                <Button>Buscar</Button>
+                <Button onClick={() => handleSubmit(onSubmit)()}>Buscar</Button>
             </div>
         </div>
     );
---
+++ src/app/trips/search/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+import Button from "@/components/Button";
+import TripItem from "@/components/TripItens";
+import { Trip } from "@prisma/client";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import React from "react";
+import { useEffect } from "react";
+
+const Trips = async () => {
+    const [trips, setTrips] = React.useState<Trip[]>([]);
+    const searchParams = useSearchParams();
+    useEffect(() => {
+        const fetchTrips = async () => {
+            const response = await fetch(`/api/trips/search?text=${searchParams.get("text") ?? ""}&startDate=${searchParams.get("startDate")}&budget=${searchParams.get("budget")}`)
+            const data = await response.json();
+            setTrips(data);
+        };
+        fetchTrips()
+    }, []);
+
+    return (
+        <div className="container mx-auto flex flex-col items-center p-5">
+            <h1 className="text-primaryDarker font-semibold text-xl">{trips.length > 0 ? "Viagens Encontradas" : "Viagens não encontradas"}</h1>
+            <h2 className="text-grayPrimary font-medium mb-5">{trips.length > 0 ? "Listamos as melhores hospedagens pra você!" : "Não encontramos nada nos seus parâmetros."}</h2>
+            {trips.length <= 0 && (
+                <Link href={"/"} className="w-full">
+                    <Button variant="outlined" className="w-full">Voltar ao início</Button>
+                </Link>
+            )}
+            <div className="flex flex-col gap-4">
+                {trips?.map(trip => (
+                    <TripItem key={trip.id} trip={trip} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Trips;
