1c94a730edb0459a80690e82d87cb77a77016644
Marcos Vieira <marcosvieira482@gmail.com>
2023-12-28 20:11:10 -0300

    style: adapt trip reservation to desktop

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -68,90 +68,92 @@ const TripReservation = ({ tripId, maxGuests, tripStartDate, tripEndDate, priceP
     const endDate = watch("endDate");
 
     return (
-        <div>
-            <div className="flex flex-col px-5">
-                <div className="flex gap-4">
-                    <Controller
-                        name="startDate"
-                        rules={{
-                            required: {
-                                value: true,
-                                message: "A data inicial é obrigatória."
-                            }
-                        }}
-                        control={control}
-                        render={({ field }) => (
-                            <DatePicker
-                                error={!!errors?.startDate}
-                                errorMessage={errors?.startDate?.message}
-                                onChange={field.onChange}
-                                selected={field.value}
-                                placeholderText="Data de Início"
-                                className="w-full"
-                                minDate={tripStartDate}
-                            />
-                        )}
-                    />
-
-                    <Controller
-                        name="endDate"
-                        rules={{
-                            required: {
-                                value: true,
-                                message: "A data final é obrigatória."
-                            }
-                        }}
-                        control={control}
-                        render={({ field }) => (
-                            <DatePicker
-                                error={!!errors?.endDate}
-                                errorMessage={errors?.endDate?.message}
-                                onChange={field.onChange}
-                                selected={field.value}
-                                placeholderText="Data Final"
-                                className="w-full"
-                                minDate={startDate ?? tripStartDate}
-                                maxDate={tripEndDate}
-                            />
-                        )}
-                    />
-                </div>
-                <Input
-                    {...register("guests", {
+        <div className="flex flex-col px-5 lg:min-w-[380px] lg:p-5 lg:border-grayLighter lg:border lg:rounded-lg lg:shadow-md">
+            <p className="text-xl hidden text-primaryDarker mb-4 lg:block">
+                <span className="font-semibold">R${pricePerDay}</span> por dia
+            </p>
+            <div className="flex gap-4">
+                <Controller
+                    name="startDate"
+                    rules={{
                         required: {
                             value: true,
-                            message: "Número de hóspedes é obrigatório."
-                        },
-                        max: {
-                            value: maxGuests,
-                            message: `Número de hospedes não pode ser maior que ${maxGuests}.`,
+                            message: "A data inicial é obrigatória."
                         }
-                    })}
-                    placeholder={`Número de hóspedes (max: ${maxGuests})`}
-                    className="mt-4"
-                    error={!!errors?.guests}
-                    errorMessage={errors?.guests?.message}
-                    type="number"
+                    }}
+                    control={control}
+                    render={({ field }) => (
+                        <DatePicker
+                            error={!!errors?.startDate}
+                            errorMessage={errors?.startDate?.message}
+                            onChange={field.onChange}
+                            selected={field.value}
+                            placeholderText="Data de Início"
+                            className="w-full"
+                            minDate={tripStartDate}
+                        />
+                    )}
                 />
-                <div className="flex justify-between mt-3">
-                    <p className="font-semibold text-md text-primaryDarker">Total:
-                        {
-                            startDate && endDate ?
-                                ` ${differenceInDays(endDate, startDate)}` : '0'
-                        } noite(s)
-                    </p>
-                    <p className="font-semibold text-md text-primaryDarker">
-                        {
-                            startDate && endDate ?
-                                `R$ ${differenceInDays(endDate, startDate) * pricePerDay}` : 'R$ 0'
+
+                <Controller
+                    name="endDate"
+                    rules={{
+                        required: {
+                            value: true,
+                            message: "A data final é obrigatória."
                         }
-                    </p>
-                </div>
-                <div className="pb-10 border-b border-b-grayLighter w-full">
-                    <Button onClick={() => handleSubmit(onSubmit)()} className="mt-3 w-full">Reservar agora</Button>
-                </div>
+                    }}
+                    control={control}
+                    render={({ field }) => (
+                        <DatePicker
+                            error={!!errors?.endDate}
+                            errorMessage={errors?.endDate?.message}
+                            onChange={field.onChange}
+                            selected={field.value}
+                            placeholderText="Data Final"
+                            className="w-full"
+                            minDate={startDate ?? tripStartDate}
+                            maxDate={tripEndDate}
+                        />
+                    )}
+                />
+            </div>
+            <Input
+                {...register("guests", {
+                    required: {
+                        value: true,
+                        message: "Número de hóspedes é obrigatório."
+                    },
+                    max: {
+                        value: maxGuests,
+                        message: `Número de hospedes não pode ser maior que ${maxGuests}.`,
+                    }
+                })}
+                placeholder={`Número de hóspedes (max: ${maxGuests})`}
+                className="mt-4"
+                error={!!errors?.guests}
+                errorMessage={errors?.guests?.message}
+                type="number"
+            />
+            <div className="flex justify-between mt-3">
+                <p className="font-semibold text-md text-primaryDarker">Total:
+                    {
+                        startDate && endDate ?
+                            ` ${differenceInDays(endDate, startDate)}` : '0'
+                    } noite(s)
+                </p>
+                <p className="font-semibold text-md text-primaryDarker">
+                    {
+                        startDate && endDate ?
+                            `R$ ${differenceInDays(endDate, startDate) * pricePerDay}` : 'R$ 0'
+                    }
+                </p>
+            </div>
+            <div className="pb-10 border-b border-b-grayLighter w-full lg:border-none lg:pb-0">
+                <Button onClick={() => handleSubmit(onSubmit)()} className="mt-3 w-full">Reservar agora</Button>
             </div>
         </div>
+
     );
 }
 
--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -20,11 +20,22 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
     if (!trip) return null;
 
     return (
-        <div className="container mx-auto">
+        <div className="container mx-auto lg:px-40">
             <TripHeader trip={trip} />
-            <TripReservation tripId={trip.id} maxGuests={trip.maxGuests} tripStartDate={trip.startDate} tripEndDate={trip.endDate} pricePerDay={Number(trip.pricePerDay)} />
-            <TripDescription description={trip.description} />
-            <TripHighlights highlights={trip.highlights} />
+            <div className="flex flex-col lg:flex-row lg:mt-5">
+                <div className="lg:order-2">
+                    <TripReservation tripId={trip.id}
+                        maxGuests={trip.maxGuests}
+                        tripStartDate={trip.startDate}
+                        tripEndDate={trip.endDate}
+                        pricePerDay={Number(trip.pricePerDay)}
+                    />
+                </div>
+                <div className="lg:order-1">
+                    <TripDescription description={trip.description} />
+                    <TripHighlights highlights={trip.highlights} />
+                </div>
+            </div>
             <TripLocation location={trip.location} locationDescription={trip.locationDescription} />
         </div>
     );
