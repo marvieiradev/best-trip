ebacd8255a934469855014df5f3dfff2fe56261c
Marcos Vieira <marcosvieira482@gmail.com>
2023-12-28 21:00:42 -0300

    style: adapt my trips page to desktop

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -28,9 +28,14 @@ const MyTrips = () => {
     }, [status]);
     return (
         <div className="container mx-auto p-5">
-            <h1 className="font-semibold text-primaryDarker text-xl">Minhas Viagens</h1>
-            {reservations.length > 0 ? (reservations?.map((reservation => <UserReservationItem key={reservation.id} fetchReservations={fetchReservations} reservation={reservation} />
-            ))) : (
+            <h1 className="font-semibold text-primaryDarker text-xl lg:mb-5">Minhas Viagens</h1>
+            {reservations.length > 0 ? (
+                <div className="flex flex-col lg:grid lg:grid-cols-3 lg:gap-12">
+                    {reservations?.map((reservation) => (
+                        <UserReservationItem fetchReservations={fetchReservations} key={reservation.id} reservation={reservation} />
+                    ))}
+                </div>
+            ) : (
                 <div className="flex flex-col mt-5">
                     <p className="mt-2 font-medium text-primaryDarker text-lg text-center">Você ainda não tem nenhuma reserva :(</p>
                     <Link href="/">
